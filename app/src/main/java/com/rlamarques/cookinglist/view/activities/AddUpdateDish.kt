933f23467debec878950485496791f0d4398ef00
rlamarques <rlamarques@hotmail.com>
2021-06-27 21:11:41 -0300

    Add menu to add new dishes
    Added a new button to the menu to add new dishes and
    create a blank activity for the add/update dishes
    that is going to be implemented

---
+++ app/src/main/java/com/rlamarques/cookinglist/view/activities/AddUpdateDish.kt
@@ -0,0 +1,12 @@
+package com.rlamarques.cookinglist.view.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.rlamarques.cookinglist.R
+
+class AddUpdateDish : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_update_dish)
+    }
+}
