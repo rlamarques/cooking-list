73ae2f8a4c26152197649a2cc6d2ab890db45894
rlamarques <rlamarques@hotmail.com>
2021-07-10 16:32:35 -0300

    Create add/update dish layout

--- app/src/main/java/com/rlamarques/cookinglist/view/activities/AddUpdateDishActivity.kt
+++ app/src/main/java/com/rlamarques/cookinglist/view/activities/AddUpdateDishActivity.kt
@@ -3,10 +3,15 @@ package com.rlamarques.cookinglist.view.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.rlamarques.cookinglist.R
+import com.rlamarques.cookinglist.databinding.ActivityAddUpdateDishBinding
 
 class AddUpdateDish : AppCompatActivity() {
+
+    private lateinit var mBinding: ActivityAddUpdateDishBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        
         setContentView(R.layout.activity_add_update_dish)
     }
 }
