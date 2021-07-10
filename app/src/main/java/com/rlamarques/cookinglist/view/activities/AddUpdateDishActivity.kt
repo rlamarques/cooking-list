6848e16ba63a6288d8c82674765bd14d62df5e88
rlamarques <rlamarques@hotmail.com>
2021-07-10 17:26:41 -0300

    Add dialog for adding new dish photos

--- app/src/main/java/com/rlamarques/cookinglist/view/activities/AddUpdateDishActivity.kt
+++ app/src/main/java/com/rlamarques/cookinglist/view/activities/AddUpdateDishActivity.kt
@@ -1,11 +1,13 @@
 package com.rlamarques.cookinglist.view.activities
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import com.rlamarques.cookinglist.R
 import com.rlamarques.cookinglist.databinding.ActivityAddUpdateDishBinding
+import com.rlamarques.cookinglist.databinding.DialogCustomImageSelectionBinding
 
 class AddUpdateDishActivity : AppCompatActivity(), View.OnClickListener {
 
@@ -21,19 +23,27 @@ class AddUpdateDishActivity : AppCompatActivity(), View.OnClickListener {
         mBinding.ivAddADishImage.setOnClickListener(this)
     }
 
+    override fun onClick(v: View?) {
+        v?.apply {
+            when(this.id) {
+                R.id.iv_add_a_dish_image -> {
+                    customImageSelectionDialog()
+                }
+            }
+        }
+    }
+
     private fun setupActionBar() {
         setSupportActionBar(mBinding.addDishToolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         mBinding.addDishToolbar.setNavigationOnClickListener { onBackPressed() }
     }
 
-    override fun onClick(v: View?) {
-        v?.apply {
-            when(this.id) {
-                R.id.iv_add_a_dish_image -> {
-                    Toast.makeText(this@AddUpdateDishActivity, "Clicked add a image", Toast.LENGTH_SHORT).show()
-                }
-            }
-        }
+    private fun customImageSelectionDialog() {
+        val dialog = Dialog(this)
+        val binding: DialogCustomImageSelectionBinding =
+            DialogCustomImageSelectionBinding.inflate(layoutInflater)
+        dialog.setContentView(binding.root)
+        dialog.show()
     }
 }
